e9742208326ffdedc5cbcfe2eb9362e82ac5786f
unknown <adityasrwt5@gmail.com>
2022-02-15 19:17:41 +0530

    basicsOfJavaCommit

---
+++ print.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args){
+        
+        int num = 7;
+        
+        System.out.println("Namaste World " + num);
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input a number :");
+        int a= scan.nextInt();
+        System.out.print("The number is :");
+        System.out.print(a);
+    }
+}
