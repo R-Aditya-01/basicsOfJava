049bb8f633c342127ab3d85e77eec0b99736a1dc
ASR <adityasrwt5@gmail.com>
2022-03-02 13:02:43 +0530

    basicsOfJava

--- PraticeQs/sfunc.java
+++ PraticeQs/sfunc.java
@@ -9,7 +9,7 @@ public class sfunc {
         return 0;
     }
     public static void main(String[] args) {
-        System.out.println("Namaste world!");
+        System.out.println("Namaste  world!");
         star(5);
     }
 }
