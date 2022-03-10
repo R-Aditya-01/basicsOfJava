6ff69ebeed519047839465b8641adb67477fe097
ASR <adityasrwt5@gmail.com>
2022-03-10 14:37:07 +0530

    basicsOfJava

--- Cyclesheet qs/q1.java
+++ Cyclesheet qs/q1.java
@@ -1,25 +1,30 @@
-import java.lang.ProcessBuilder.Redirect.Type;
-import java.util.Scanner;
 
-import org.w3c.dom.TypeInfo;
+import java.util.Scanner;
 
 public class q1 {
 	public static void main(String[] args) {
-		System.out.println("Namaste World");
+		// System.out.println("Namaste World");
 		Scanner scc = new Scanner(System.in);
 		int num = scc.nextInt();
-		int ic = 0, fc = 0, stc = 0, csum = 0;
+		int ic = 0, fc = 0, stc = 0, s = Integer.MAX_VALUE, l = Integer.MIN_VALUE;
+		float csum = 0;
+		String con = " ";
 
 		for (int i = 0; i < num; i++) {
 
 			Scanner sc1 = new Scanner(System.in);
-			// int rn = sc.nextInt();
-			// float cgpa = sc.nextFloat();
-			// String nam = sc.nextLine();
 
 			if (sc1.hasNextInt()) {
 				System.out.println("int");
 				int rn = sc1.nextInt();
+				if (rn < s) {
+					s = rn;
+				}
+
+				if (rn > l) {
+					l = rn;
+				}
+
 				ic++;
 			} else if (sc1.hasNextFloat()) {
 				System.out.println("float");
@@ -31,14 +36,18 @@ public class q1 {
 				System.out.println("string");
 				stc++;
 				String nam = sc1.nextLine();
+				con += nam + ",";
 			}
+			sc1.close();
 		}
 
-		double avg = csum / fc;
+		float avg = csum / fc;
 
-		System.out.print(ic + "\n" + fc + "\n" + stc + "\n" + avg);
-		// System.out.printf(%.3f %n,avg);
+		System.out.println(ic + "\n" + fc + "\n" + stc + "\n" + avg);
+		System.out.println(s + "\n" + l);
+		System.out.println(con);
 
-		// sc.close();4
+		scc.close();
+
 	}
 }
