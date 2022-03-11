d8ccd65fcd6034a531f32fb3e95ec755d57aedcc
ASR <adityasrwt5@gmail.com>
2022-03-11 13:01:21 +0530

    basicsOfJava

--- Cyclesheet qs/q6.java
+++ Cyclesheet qs/q6.java
@@ -5,11 +5,13 @@ public class q6 {
     public static void main(String Arg[]) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+        str = str.toUpperCase();
         String a = "";
         boolean flag = false;
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
+
             if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
                 flag = true;
                 a += "V";
